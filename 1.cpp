f63749fa5776c4cc1cb8937c9074a6b7c58fcade
michal <safurynm@wp.pl>
2022-11-16 17:02:57 +0100

    4th commit

--- 1.cpp
+++ 1.cpp
@@ -5,41 +5,68 @@
 
 using namespace std;
 
+void allPermutations(int*, int);
+void randomPermutations(int*, int, int);
+
 int main(int argc, char* argv[])
 {
-    // Zmienne
-    int loopIter = 0;
-    int arraySize = atoi(argv[1]);
-    int perCount = atoi(argv[2]);
-
-    // Dynamiczne alkowanie tablicy
-    int *tab = new int[arraySize];
-
-    // Sprawdzenie danych wejsciowych
-    if (perCount > 10000)
+    for (int i  = 0; i < atoi(argv[1]); i++)
     {
-        cout << "Za duzo permutacji zostalo podanych" << endl;
-        return 0;
+        // Variables
+        int loopIter = 0;
+        int arraySize = atoi(argv[2 + (2 * i)]);
+        int perCount = atoi(argv[3 + (2 * i)]);
+
+        int *tab = new int[arraySize];
+
+
+        // Checking input
+        if (perCount > 10000)
+        {
+            cout << "Za duzo permutacji zostalo podanych" << endl;
+            return 0;
+        }
+
+        // Filling an array
+        srand(time(NULL));
+        for (int i = 1; i <= arraySize; i++)
+            tab[i-1] = i;
+
+
+        // Permutations
+        if ((*argv[3 + (2 * i)]) == 'A')
+            allPermutations(tab, arraySize);
+        else 
+            randomPermutations(tab, perCount, arraySize);
+
+
+        delete[] tab;
     }
+  return 0;
+}
 
-
-    // Wypelnianaie tablicy do pierwszego parametru
-    for (int i = 1; i <= arraySize; i++)
-        tab[i-1] = i;
-
-    // Petla permutujaca tablice
+void allPermutations(int* tab, int arraySize)
+{
+    int loopIter = 0;
     do
+        {
+            for (int i = 1; i <= arraySize; i++)
+                cout << tab[i-1] << " ";
+            cout << endl;
+            loopIter++;
+        } while (next_permutation(tab, tab + arraySize));
+    
+    cout << "Ilosc wykonanych permutacji "  << endl;
+    cout << loopIter << endl;
+}
+
+void randomPermutations(int* tab, int perCount, int arraySize)
+{
+    for (int i = 0; i < perCount; i++)
     {
+        random_shuffle(tab, tab+arraySize);
         for (int i = 1; i <= arraySize; i++)
             cout << tab[i-1] << " ";
         cout << endl;
-
-        loopIter++;
-        if (loopIter == perCount)
-            break;
-
-    } while (next_permutation(tab, tab + arraySize));
-
-    delete[] tab;
-  return 0;
+    }
 }
