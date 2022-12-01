2ed6e6fae31ec2c3c9870e16e57a7b53a99c7add
pawelktk <pawel.klopotek@gmail.com>
2022-12-01 01:47:19 +0100

    Updated config file error handler

--- config_error_handler.sh
+++ config_error_handler.sh
@@ -5,14 +5,14 @@ max_set_size="$2"
 
 #config file doesn't exist
 if [ ! -f "$config_file" ]; then
-  echo error_file_does_not_exist
+  echo "Error: file doesn't exist"
   exit 
 fi
 
 #number of lines < 2
 number_of_lines=$(wc -l "$config_file" | awk '{print $1}')
 if ((number_of_lines<2)); then
-  echo error_input_incomplete
+  echo "Error: input incomplete"
   exit
 fi
 
@@ -21,7 +21,7 @@ declared_number_of_lines=$(head -n 1 "$config_file")
 #declared_number_of_lines is not a valid number
 
 if ! [[ $declared_number_of_lines =~ ^[0-9]+$ ]] ; then
-  echo error_not_a_valid_number
+  echo "Error in line 1: not a valid number"
   exit
 fi
 
@@ -31,15 +31,15 @@ fi
 
 #syntax error: $current_line doesn't match regex [0-9A]+\s[0-9]+  
 counter=0
-for ((i=2;i<=declared_number_of_lines;i++)); do
+for ((i=2;i<=declared_number_of_lines+1;i++)); do
   current_line="$(awk "NR==$i" "$config_file")"
-  if ! [[ "$current_line" =~ [0-9]+\s[0-9A]+ ]] ; then
-    echo error_syntax_error
+  if ! [[ "$current_line" =~ [0-9]+' '[0-9A]+ ]] ; then
+    echo "Error in line $i: syntax error"
     exit
   fi
   set_size="$(echo $current_line | awk '{print $1;}')"
   if ((set_size>max_set_size)); then
-    echo error_set_too_big
+    echo "Error in line $i: set size is higher than the maximum set limit($max_set_size)"
     exit
   fi
 
@@ -49,7 +49,7 @@ done
 # $declared_number_of_lines doesn't match
 
 if ((counter!=declared_number_of_lines)); then
-  echo error_lines_dont_match
+  echo "Error: declared number of parameters doesn't match. Expected: $declared_number_of_lines. Got: $counter"
   exit
 fi
 
